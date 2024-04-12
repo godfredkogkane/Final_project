66da9e0a46d69077d68f9c9c89d77fda35cab0a7
Regina Lettey <letteyregina2002@gmail.com>
2024-04-12 20:52:41 +0000

    styled some pages

--- view/feedback.php
+++ view/feedback.php
@@ -8,11 +8,11 @@
 </head>
 <body>
     <header>
-        <h1>Submit Feedback</h1>
+        <h1 style="color: black;">Submit Feedback</h1>
     </header>
 
     <section>
-        <h1 style="color: rgb(33, 214, 33);">We'd love to hear from you!</h1>
+        <h1 style="color:black;">We'd love to hear from you!</h1>
         <form action="../actions/feedback_action.php" method="POST">
             <textarea name="message" rows="4" cols="50" placeholder="Enter your feedback here..." required></textarea><br>
             <input type="hidden" name="date" value="<?php echo date('Y-m-d'); ?>">
