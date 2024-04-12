e83d523b711f24b19886786ff71931fa2740bc31
Regina Lettey <letteyregina2002@gmail.com>
2024-04-12 13:02:24 +0000

    edited feedback_display

--- view/feedback_display.php
+++ view/feedback_display.php
@@ -1,10 +1,19 @@
+<?php include "feedback_display_actions.php";
+// Include database connection file
+include "../settings/connection.php";
+
+// Retrieve feedback entries from the database along with the user's name
+$sql = "SELECT Feedback.Message, Feedback._Date, Users.Firstname, Users.Lastname FROM Feedback JOIN Users ON Feedback.UserID = Users.UserID ORDER BY Feedback._Date DESC";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Feedback</title>
-    <link rel="stylesheet" href="../css/feedback.css"> 
+    <!-- <link rel="stylesheet" href="../css/feedback.css">  -->
 </head>
 <body>
     <header>
