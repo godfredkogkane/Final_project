88565e0899e7ca3aeeea4aca61d06ba954cb5721
Regina Lettey <letteyregina2002@gmail.com>
2024-04-12 02:14:45 +0000

    added new page

---
+++ actions/feedback_display_action.php
@@ -0,0 +1,8 @@
+<?php
+// Include database connection file
+include "../settings/connection.php";
+
+// Retrieve feedback entries from the database along with the user's name
+$sql = "SELECT Feedback.Message, Feedback._Date, Users.Firstname, Users.Lastname FROM Feedback JOIN Users ON Feedback.UserID = Users.UserID ORDER BY Feedback._Date DESC";
+$result = $conn->query($sql);
+?>
